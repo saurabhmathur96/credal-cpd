fec4b14f647c919a8e50c379b824333817439455
Saurabh Mathur <saurabhmathur96@gmail.com>
2023-07-03 12:24:23 -0500

    Update dataset.py

--- dataset.py
+++ dataset.py
@@ -1,7 +1,6 @@
 import pandas as pd 
 import numpy as np 
 from sklearn.model_selection import train_test_split
-from pgmpy.factors.discrete import TabularCPD
 from sklearn.preprocessing import KBinsDiscretizer
 from os import path 
 
